244072591f1079fe684435ce90dd18c843be49c7
Your Name <you@example.com>
2025-10-16 10:50:16 -0300

    Svelte!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+	let cont: number = $state(0);
+	abstract class Incrementador
+	{
+		public static incrementa(): void
+		{
+			cont ++;
+		}
+	}
+</script>
+
+<button onclick={Incrementador.incrementa}>
+	Clicou {cont} vezes.
+</button>
+
+{#if cont > 10}
+	<p>Você REALMENTE gosta MUITO de clicar!</p>
+{:else if cont < 5}
+	<p>Nada a declarar.</p>
+{:else}
+	<p>Você gosta de clicar!</p>
+{/if}
